78f057a8f84c87f00d191dcbd59db064225061a5
Jakub Rozmiarek <j.rozmiarek@psp-polska.pl>
2013-03-27 17:30:11 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EspagoMemes::Application.config.secret_token = '80b94a58703b4d6d685e4cc1d226254c10f155909b30db240fb4f7254c604e5bdcf01ab94328a77928845259764f913779496ab829c305eb9254ffd238dcafe0'
