774e0fd7678ba0aa6981ee4d6f8dbafe0cbe44c2
Jakub Rozmiarek <j.rozmiarek@psp-polska.pl>
2013-03-30 20:54:29 +0100

    like and unlike refactorized

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,21 +2,12 @@ class LikesController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    meme = Meme.find(params[:id])
-    unless current_user.likes_meme?(meme)
-      like = current_user.likes.build
-      like.subject = meme
-      like.save
-      meme.update_column(:likes, meme.likes.count)
-    end
+    Like.create!(current_user, params)
     redirect_to :back
   end
 
   def destroy
-    meme = Meme.find(params[:id])
-    like = current_user.likes_meme?(meme)
-    like.destroy unless like.nil?
-    meme.update_column(:likes, meme.likes.count)
+    Like.destroy!(current_user, params)
     redirect_to :back
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,21 @@ class Like < ActiveRecord::Base
   belongs_to :subject, polymorphic: true
 
   default_scope order('created_at ASC')
+
+  def self.create!(user, params)
+    meme = Meme.find(params[:id])
+    unless user.likes_meme?(meme)
+      like = user.likes.build
+      like.subject = meme
+      like.save
+      meme.update_column(:likes, meme.likes.count)
+    end
+  end
+
+  def self.destroy!(user, params)
+    meme = Meme.find(params[:id])
+    like = user.likes_meme?(meme)
+    like.destroy unless like.nil?
+    meme.update_column(:likes, meme.likes.count)
+  end
 end
